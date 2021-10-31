861854ed3ca9d03ab394617b1f4b852e1a214ed6
ScriptMan404 <45128030+ScriptMan404@users.noreply.github.com>
2021-10-31 15:05:52 +0100

    Add files via upload

---
+++ WIP-ProvisionedAppsDecrapifier.ps1
@@ -0,0 +1,5 @@
+﻿$Apps2Uninstall = @('*Office*','*todos*','*windowscommunicationsapps*','*MicrosoftTeams*','*WindowsMaps*','*BingNews*','*BingWeather*','*GetHelp*','*MixedReality*','*Getstarted*','*Microsoft3DViewer*','*MicrosoftSolitaireCollection*','*PowerAutomateDesktop*','*ScreenSketch*','*SkypeApp*','*Wallet*','*WindowsFeedbackHub*','*People*')
+
+foreach ($Apps in $Apps2Uninstall) {
+    Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -like $Apps} | Remove-AppxProvisionedPackage -Online -AllUsers
+}
