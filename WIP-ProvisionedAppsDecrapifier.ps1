57674a9e48dcdb9bbc9aaa01dcf65af3857dec8c
ScriptMan404 <45128030+ScriptMan404@users.noreply.github.com>
2022-04-26 21:38:13 +0200

    Delete WIP-ProvisionedAppsDecrapifier.ps1

--- WIP-ProvisionedAppsDecrapifier.ps1
+++
@@ -1,5 +0,0 @@
-﻿$Apps2Uninstall = @('*Office*','*todos*','*windowscommunicationsapps*','*MicrosoftTeams*','*WindowsMaps*','*BingNews*','*BingWeather*','*GetHelp*','*MixedReality*','*Getstarted*','*Microsoft3DViewer*','*MicrosoftSolitaireCollection*','*PowerAutomateDesktop*','*ScreenSketch*','*SkypeApp*','*Wallet*','*WindowsFeedbackHub*','*People*')
-
-foreach ($Apps in $Apps2Uninstall) {
-    Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -like $Apps} | Remove-AppxProvisionedPackage -Online -AllUsers
-}
