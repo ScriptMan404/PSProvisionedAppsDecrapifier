25ea2250ba259a480e01064583c41dd27d3a0691
ScriptMan404 <45128030+ScriptMan404@users.noreply.github.com>
2022-08-27 12:06:36 +0200

    Add files via upload

---
+++ WIP-PSProvisionedAppsDecrapifier.ps1
@@ -0,0 +1,34 @@
+﻿$APPS2Uninstall = @(
+    'Office',
+    'todos',
+    'windowscommunicationsapps',
+    'MicrosoftTeams',
+    'WindowsMaps',
+    'BingNews',
+    'BingWeather',
+    'GetHelp',
+    'MixedReality',
+    'Getstarted',
+    'Microsoft3DViewer',
+    'MicrosoftSolitaireCollection',
+    'PowerAutomateDesktop',
+    'SkypeApp',
+    'Wallet',
+    'WindowsFeedbackHub',
+    'People',
+    'Zune',
+    'MSPaint',
+    'YourPhone'
+)
+
+$TOTALItems = $APPS2Uninstall.Count
+$CURRENTItem = 0
+$PERCENTComplete = 0
+
+foreach ($APPS in $APPS2Uninstall) {
+    Write-Progress -Activity "REMOVING PROVISIONED APPS..." -Status "$PERCENTComplete% Complete:" -PercentComplete $PERCENTComplete
+    Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -match $APPS} | Remove-AppxProvisionedPackage -Online -AllUsers
+    $CURRENTItem++
+    $PERCENTComplete = [int](($CURRENTItem / $TOTALItems) * 100)
+    Start-Sleep -Milliseconds 200
+}
